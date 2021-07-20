69be52ce7aaf95fd71398370df0b2a5f5fecbaca
DESKTOP-M5RES2E\ptooo <ptoooool@naver.com>
2021-07-20 22:27:13 +0900

    feat:rabbitMQ

---
+++ src/main/java/class101/foo/io/Consumer.java
@@ -0,0 +1,23 @@
+package class101.foo.io;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @RabbitListener(queues = "POST_QUEUE") //이 큐를 계속 컨슘하고 있는 상태로 큐에 메세지가 들어오면 이 메소드사 실행된다.
+    public void handler(String message) throws JsonProcessingException {
+        //문자열을 읽어와서 json으로 올텐데 Post타입으로 변경해주며 컨트롤러 있던 것처럼 save를 해주면 메세지 큐에 있는 내용이 DB에 들어간다.
+        Post post = objectMapper.readValue(message, Post.class);
+        postRepository.save(post);
+    }
+}
---
+++ src/main/java/class101/foo/io/Producer.java
@@ -0,0 +1,16 @@
+package class101.foo.io;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+// 글작성이 왔을 때 이 코드를 실행해서 RabbitMQ에다가 메세지를 넣어준다.
+@Component
+public class Producer {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendTo(String message){
+        this.rabbitTemplate.convertAndSend("POST_QUEUE", message);
+    }
+}
