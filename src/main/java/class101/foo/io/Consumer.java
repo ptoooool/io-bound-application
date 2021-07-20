26a90dfe936a0526ca00da28a50da1ba50da0604
DESKTOP-M5RES2E\ptooo <ptoooool@naver.com>
2021-07-20 22:43:30 +0900

    feat

--- src/main/java/class101/foo/io/Consumer.java
+++ src/main/java/class101/foo/io/Consumer.java
@@ -14,7 +14,7 @@ public class Consumer {
     @Autowired
     PostRepository postRepository;
 
-    @RabbitListener(queues = "POST_QUEUE") //이 큐를 계속 컨슘하고 있는 상태로 큐에 메세지가 들어오면 이 메소드사 실행된다.
+    @RabbitListener(queues = "CREATE_POST_QUEUE") //이 큐를 계속 컨슘하고 있는 상태로 큐에 메세지가 들어오면 이 메소드사 실행된다.
     public void handler(String message) throws JsonProcessingException {
         //문자열을 읽어와서 json으로 올텐데 Post타입으로 변경해주며 컨트롤러 있던 것처럼 save를 해주면 메세지 큐에 있는 내용이 DB에 들어간다.
         Post post = objectMapper.readValue(message, Post.class);
